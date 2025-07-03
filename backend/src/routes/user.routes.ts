1387398972f0e2534eab1166d791a40febf74e6a
manish076 <manishku073@gmail.com>
2025-07-03 16:27:58 +0530

    feat : be getUserbyid And getUserByfirstName And LastName

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getDetails, ProfileUpdate, siginInController, singUpController } from "../controller/user.controller";
 import userAuthCheck from "../midddleware/userAuth";
+import { filterUserByName, getUserById } from "../controller/account.controller";
 
 const router=Router();
 
@@ -8,6 +9,8 @@ router.post("/signin",siginInController);
 router.post("/signup",singUpController);
 router.put("/updateprofile",userAuthCheck,ProfileUpdate);
 router.get("/getuser",userAuthCheck,getDetails)
+router.get("/getuserbyid",userAuthCheck,getUserById);
+router.get("/filter",userAuthCheck,filterUserByName)
 
 
 export default router
